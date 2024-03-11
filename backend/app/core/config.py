db9085922aa6d7e825c01671d1f3331766bfb596
AlexDyakonov <shram.monolit@mail.ru>
2024-03-11 20:36:07 +0300

    Added alembic migrations

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -7,10 +7,12 @@ load_dotenv()
 POSTGRES_DB = os.getenv("POSTGRES_DB", "database")
 POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "root")
-POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+POSTGRES_HOST = "localhost"
 POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
 
 PG_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+print(PG_DATABASE_URL)
 
 
 class Config:
